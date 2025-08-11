4dd3a4314c1822e0e1958ca0566d3c44cd58b29f
destiny28-ed <destinyijeoma28@gmail.com>
2025-08-11 21:25:53 +0200

    Add project files.

---
+++ Tutorial14/Controllers/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial14.Controllers
+{
+    public static class Globals
+    {
+        public static string ConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Social_Club_Membership;Integrated Security=True;Encrypt=False";
+
+
+    }
+}
---
+++ Tutorial14/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tutorial14.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection myConnection = new SqlConnection(Globals.ConnectionString);
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        public ActionResult DoInsert(string name, string club_name, string age,decimal fee)
+        {
+            try
+            {
+                SqlCommand myInsertCommand = new SqlCommand("Insert into Membership Values('" + name+"' ,'" + club_name+"','" + age+"'," + fee+" )", myConnection);
+
+                myConnection.Open();
+                int rowsAffected = myInsertCommand.ExecuteNonQuery();
+                ViewBag.Message = "Success: " + rowsAffected + " rows added.";
+            }
+            catch (Exception err)
+            {
+                ViewBag.Message = "Error: " + err.Message;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return View("Index");
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult DoUpdate(int id, string name)
+        {
+            try
+            {
+                SqlCommand myUpdateCommand = new SqlCommand("Update Membership Set Member_Name='" + name+ "' where Member_ID=" + id, myConnection);
+
+                myConnection.Open();
+                int rowsAffected = myUpdateCommand.ExecuteNonQuery();
+                ViewBag.Message = "Success: " + rowsAffected + " rows updated.";
+            }
+            catch (Exception err)
+            {
+                ViewBag.Message = "Error: " + err.Message;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return View("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult DoDelete(int id)
+        {
+            try
+            {                
+                SqlCommand myDeleteCommand = new SqlCommand("Delete from Membership where Member_ID=" + id, myConnection);
+
+                myConnection.Open();
+                int rowsAffected = myDeleteCommand.ExecuteNonQuery();
+                ViewBag.Message = "Success: "+ rowsAffected + " rows deleted.";
+            }
+            catch (Exception err)
+            {
+                ViewBag.Message = "Error: " + err.Message;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return View("Index");
+        }
+
+
+    }
+}
